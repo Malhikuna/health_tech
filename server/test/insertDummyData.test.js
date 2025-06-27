2eb29868f4c086544030e0cbc4e2fc9b1c686c98
hikmalmn <hikmal.233040091@mail.unpas.ac.id>
2025-06-27 22:32:09 +0700

    fix: perbaiki file auth.service.js

--- server/test/insertDummyData.test.js
+++ server/test/insertDummyData.test.js
@@ -15,84 +15,35 @@ import {
   removeUserActivity,
   createDailyPlan,
   removeDailyPlan,
-  getProgram, getUserProgram, createUserActivity
+  createUserActivity,
 } from "./test-util.js";
-import supertest from "supertest";
-import {web} from "../src/application/web.js";
-import {logger} from "../src/application/logging.js";
 
-describe('GET /api/progress/log-meal', function () {
+describe("Insert Data", function () {
   beforeEach(async () => {
     await createTestUser();
     await createProgram();
     await createUserProgram();
+    await createUserActivity();
     await createRecipes();
     await createIngredients();
     await createRecipeIngredients();
     await createDailyPlan();
-  })
+  });
 
-  afterEach(async () => {
+  it("should can insert data", async () => {});
+});
+
+describe("Delete Data", function () {
+  beforeEach(async () => {
     await removeDailyPlan();
     await removeRecipeIngredients();
-    await removeIngredients()
+    await removeIngredients();
     await removeRecipes();
     await removeUserActivity();
     await removeUserProgram();
     await removeProgram();
     await removeTestUser();
-  })
-
-  it('should can log meal', async () => {
-    const token = await getToken();
-    const {program_id: programId} = await getUserProgram();
-
-    const result = await supertest(web)
-      .post('/api/progress/log-meal')
-      .set('Authorization', `Bearer ${token}`)
-      .send({
-        "programId": programId,
-        "dayNumber": 1,
-        "mealType": "breakfast",
-        "completed": 1
-      })
-
-    logger.info(result.body);
-
-    expect(result.status).toBe(200);
   });
-})
 
-describe('Insert Data', function () {
-  beforeEach(async () => {
-    await createTestUser();
-    await createProgram();
-    await createUserProgram();
-    await createUserActivity()
-    await createRecipes();
-    await createIngredients();
-    await createRecipeIngredients();
-    await createDailyPlan();
-  })
-
-  it('should can insert data', async () => {
-
-  });
-})
-
-describe('Delete Data', function () {
-  beforeEach(async () => {
-    await removeDailyPlan();
-    await removeRecipeIngredients();
-    await removeIngredients()
-    await removeRecipes();
-    await removeUserActivity();
-    await removeUserProgram();
-    await removeProgram();
-    await removeTestUser();
-  })
-
-  it('should can insert data', async () => {
-
-  });
-})
+  it("should can insert data", async () => {});
+});
